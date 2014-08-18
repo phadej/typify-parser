01c9d24a1354ce4dffb1c7ecfa54d8d01cee7a49
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-08-18 06:55:08 +0300

    Implementation

---
+++ test/errors.js
@@ -0,0 +1,32 @@
+/* global describe:true, it:true */
+"use strict";
+
+var parser = require("../lib/parser.js");
+var expect = require("chai").expect;
+
+function fixture(signature) {
+  expect(function() {
+    parser(signature);
+  }).to.throw();
+}
+
+describe("errorneous cases", function () {
+  it("lexing error", function () {
+    fixture("/");
+  });
+
+  it("unclosed bracket", function () {
+    fixture("[ 1");
+  });
+
+  it("typos in record", function () {
+    fixture("{");
+    fixture("{ :");
+    fixture("{ foo: bar");
+    fixture("{ foo ; bar }");
+  });
+
+  it("traling data", function () {
+    fixture("aa ab ;");
+  });
+});
