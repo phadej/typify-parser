c7c59d4772878b4a5b4cf60befed0a4b95339feb
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-08-18 06:26:08 +0300

    Fixture tests

--- test/fixtures.js
+++ test/fixtures.js
@@ -2,10 +2,24 @@
 "use strict";
 
 var parser = require("../lib/parser.js");
-var assert = require("assert");
+var expect = require("chai").expect;
+var fs = require("fs");
+var path = require("path");
 
 describe("fixtures", function () {
-  it("implement me", function () {
-    assert(parser("foobar").length === 0);
+  var dirname = path.join(__dirname, "fixtures");
+  var files = fs.readdirSync(dirname);
+  files.forEach(function (file) {
+    it(file, function () {
+      var contents = fs.readFileSync(path.join(dirname, file)).toString();
+      var lines = contents.split("\n");
+      expect(lines.length).to.equal(3);
+      expect(lines[2]).to.equal("");
+
+      var signature = lines[0];
+      var json = JSON.parse(lines[1]);
+
+      expect(parser(signature)).to.deep.equal(json);
+    });
   });
 });
